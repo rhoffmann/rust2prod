fd039600756fe8079d7db42cf1237902379cbac3
Richard Hoffmann <rhoffmann@fastmail.com>
2024-03-15 22:12:15 +0100

    update configuration management and add sqlx prepare statements for offline use

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM rust:1.76.0
+LABEL authors="richard"
+
+WORKDIR /app
+RUN apt update && apt install -y lld clang
+
+COPY . .
+ENV SQLX_OFFLINE true
+RUN cargo build --release
+ENTRYPOINT ["/app/target/release/rust2prod"]
--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,10 @@ async fn main() -> Result<(), std::io::Error> {
     // application code
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(&address).expect("Failed to bind port");
 
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to postgres.");
 
     println!("running on http://{}", &address);
