4f94e48612a61fb28cfcae0a5ce4d679dfdee49d
Richard Hoffmann <rhoffmann@fastmail.com>
2024-03-16 20:33:28 +0100

    implement try from for subscriber

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,21 @@ pub struct SubscribeFormData {
     name: String,
 }
 
+
+impl TryFrom<SubscribeFormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(data: SubscribeFormData) -> Result<Self, Self::Error> {
+        let name =  SubscriberName::parse(data.name)?;
+        let email = SubscriberEmail::parse(data.email)?;
+
+        Ok(Self {
+            email,
+            name
+        })
+    }
+}
+
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -24,19 +39,10 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
 
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    // try into here is a trait that is implemented by TryFrom for the NewSubscriber struct
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber {
-        email,
-        name
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
