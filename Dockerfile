4ef2850d6eea67685ead685280bc9eefcc7c2b24
Richard Hoffmann <rhoffmann@fastmail.com>
2024-03-16 10:44:30 +0100

    make environment-able configurations

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN apt update && apt install -y lld clang
 COPY . .
 ENV SQLX_OFFLINE true
 RUN cargo build --release
+ENV APP_ENVIRONMENT production
+
 ENTRYPOINT ["/app/target/release/rust2prod"]
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,4 @@
-use config::Config;
+use config::{Config};
 use secrecy::{ExposeSecret, Secret};
 
 #[derive(serde::Deserialize)]
@@ -22,6 +22,35 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+pub enum Environment {
+    Local,
+    Testing,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Local => "local",
+            Self::Testing => "testing",
+            Self::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "testing" => Ok(Self::Testing),
+            "production" => Ok(Self::Production),
+            other => Err(format!("{} is not a valid environment", other)),
+        }
+    }
+}
+
 impl DatabaseSettings {
     pub fn connection_string(&self) -> Secret<String> {
         Secret::new(format!("{}/{}", self.connection_string_without_db().expose_secret(), self.database_name))
@@ -35,13 +64,21 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let builder =
-        Config::builder().add_source(config::File::new("configuration", config::FileFormat::Json));
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
 
-    // TODO: add environment specific file handling, e.g. a different database server for testing?
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
 
-    // look for 'configuration' top level file (yaml, json, etc)
-    let s = builder.build()?;
+    let environment_filename = format!("{}.yaml", environment.as_str());
 
-    s.try_deserialize()
+    let settings = Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base.yaml")).required(true))
+        .add_source(config::File::from(configuration_directory.join(environment_filename)).required(true))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
 }
+
