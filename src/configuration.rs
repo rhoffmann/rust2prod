4536e9d94f8aea77570276902a2ee0599bb442a6
Richard Hoffmann <rhoffmann@fastmail.com>
2024-03-16 14:25:57 +0100

    add deserialize from env vars

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,6 @@
 use config::{Config};
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -9,6 +10,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -17,6 +19,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -77,6 +80,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = Config::builder()
         .add_source(config::File::from(configuration_directory.join("base.yaml")).required(true))
         .add_source(config::File::from(configuration_directory.join(environment_filename)).required(true))
+        // we can also add settings from environment variables e.g. settings.application.port with APP_APPLICATION__PORT=5001
+        .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
         .build()?;
 
     settings.try_deserialize::<Settings>()
