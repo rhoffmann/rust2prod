ba044692b69fc87533dc883031b065f2e6abc59c
Richard Hoffmann <rhoffmann@fastmail.com>
2024-04-01 13:18:36 +0200

    move auth code to lib

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,15 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+
+use secrecy::Secret;
+
+#[derive(serde::Deserialize)]
+pub struct LoginData {
+    pub email: String,
+    pub password: Secret<String>,
+}
 
 // returns htmx fragment
-pub async fn login_post_fragment() -> HttpResponse {
+pub async fn login_post_fragment(_form: web::Form<LoginData>) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header(("HX-Redirect", "/"))
         // .insert_header((LOCATION, "/")) // this works only without htmx
